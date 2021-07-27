32a4a99bb668df789e827a20d8c1fb0f6578f4ea
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-26 20:56:52 -0500

    css

--- src/scripts/user_info.js
+++ src/scripts/user_info.js
@@ -1,3 +1,17 @@
 export const receiveUser = () => {
-  
+  let userInfo = document.getElementById("user-info")
+  let submit = document.getElementById("submit-info")
+
+
+  userInfo.onsubmit = function(e) {
+    e.preventDefault()
+    userInfo.style.webkitAnimation = "move-left 1.5s"
+    userInfo.style.webkitAnimationTimingFunction = "ease-in-out "
+    submit.style.webkitAnimation = "disappear 1.5s"
+    submit.style.webkitAnimationTimingFunction = "ease-out"
+    setTimeout(() => {
+      userInfo.style.transform = "translate(-150%, -50%)"
+      submit.style.display = "none"
+    }, 1500)
+  }
 }
