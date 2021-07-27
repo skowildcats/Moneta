9d627c92e0744e9ac282d96605f86ca7f916185c
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-26 20:02:05 -0500

    css

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -1,3 +1,5 @@
+import { receiveUser } from "./user_info";
+
 export const startDemo = () => {
   const canvas = document.getElementById("canvas");
   canvas.height = window.innerHeight
@@ -6,13 +8,23 @@ export const startDemo = () => {
 
   canvasCtx.fillStyle = 'black';
   canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
+
+
   let start = document.getElementById("start")
   let introduction = document.getElementById("introduction")
+  let userInfo = document.getElementById("user-info")
 
   start.onclick = function() {
-    introduction.style.webkitAnimation = "disappear 2s"
+    introduction.style.webkitAnimation = "disappear 1.5s"
+    introduction.style.webkitAnimationTimingFunction = "ease-out"
+
+    userInfo.style.webkitAnimation = "appear 3.5s"
+    userInfo.style.webkitAnimationTimingFunction = "ease-in"
+    userInfo.style.display = "flex"
     setTimeout(() => {
       introduction.style.display = "none"
-    }, 2000);
+    }, 1500);
+    receiveUser()
   }
+
 }
