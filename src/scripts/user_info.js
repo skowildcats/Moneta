4ca0cf13affc19b10bc42e13c80480a1c936a77e
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-27 17:32:53 -0500

    css

--- src/scripts/user_info.js
+++ src/scripts/user_info.js
@@ -4,14 +4,17 @@ export const receiveUser = () => {
   let userInfo = document.getElementById("user-info")
   let submit = document.getElementById("submit-info")
   let infoHeader = document.getElementById("info-header")
+  let stocks = document.getElementById("stocks")
 
 
   userInfo.onsubmit = function(e) {
     e.preventDefault()
+    appear(stocks, 1.5)
     userInfo.style.webkitAnimation = "move-left 1.5s"
     userInfo.style.webkitAnimationTimingFunction = "ease-in-out"
     disappear(submit, 1.5)
     disappear(infoHeader, 1.5)
+    stocks.style.display = "block"
     
     setTimeout(() => {
       userInfo.style.transform = "translate(-250%, -50%)"
